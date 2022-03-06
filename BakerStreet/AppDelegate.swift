97c94b0c369dd8a3318d7bdda5c43760a99bcedf
Ian Hocking <ihocking@gmail.com>
2022-03-06 10:43:40 +0000

    Initial commit

--- BakerStreet/AppDelegate.swift
+++ BakerStreet/AppDelegate.swift
@@ -25,9 +25,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-
-
-        
+       
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
